dbbc2652934b191ac3f367692731e2ff09abdcd3
“DanielMOli” <arayty0suave@gmail.com>
2022-02-15 19:38:01 -0300

    Terceiro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+state = {
+  name: "Daniel",
+  age: 16,
+  food: "Mocotó",
+  music: [ "SuperGirl - Aimyon", "Oidar - Hether", "Quatro Estações - Kleber Lucas" ]
 }
 
+ render() {
+   return (
+     <div>
+         <h1> Meu nome é {this.state.name}</h1>
+         <h2> Tenho {this.state.age} anos</h2>
+       <h3> Gosto de {this.state.food} </h3>
+       <ol> 
+         <li>{this.state.music[0]}</li>
+         <li>{this.state.music[1]}</li>
+         <li>{this.state.music[2]}</li>
+         </ol>
+     </div>
+   );
+ }
+}
 export default App;
